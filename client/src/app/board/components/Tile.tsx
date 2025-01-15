943156290e64e61048f3e32a081cd990dee12e92
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2025-01-15 16:50:29 +0100

    changed pawns folder localizations

--- client/src/app/board/components/Tile.tsx
+++ client/src/app/board/components/Tile.tsx
@@ -1,11 +1,10 @@
-
 interface Props {
     number: number;
 }
 
 export default function Tile({ number }: Props) {
     if(number % 2 === 0) {
-        return <div className="w-[100px] h-[100px] bg-[#a16f5a]"></div>
+        return <div className="w-[100px] h-[100px] bg-[#a16f5a]"><img src="../pawns/BlackPawn.svg"/></div>
     }
     else {
         return <div className="w-[100px] h-[100px] bg-[#ecd3b8]"></div>
--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -3,11 +3,13 @@ import Chessboard from './components/Chessboard';
 
 const Board: NextPage = () => {
     return (
-        <div>
-            <div className="grid place-content-center h-screen bg-[#1a1a1a]">
-                <Chessboard />
-            </div>    
-        </div>
+        <>
+            <div>
+                <div className="grid place-content-center h-screen bg-[#1a1a1a]">
+                    <Chessboard />
+                </div>    
+            </div>
+        </>
     );
 };
 
