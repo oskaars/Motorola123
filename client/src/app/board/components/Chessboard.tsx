dd89b25743e0e35a8364035b2d363f19d6fd9d6e
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2025-01-15 17:09:15 +0100

    pieces placing work

--- client/src/app/board/components/Chessboard.tsx
+++ client/src/app/board/components/Chessboard.tsx
@@ -3,14 +3,28 @@ import Tile from "./Tile";
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+interface Piece {
+    image: string;
+    x: number;
+    y: number;
+}
+
+const pieces: Piece[] = [];
+
+pieces.push({iamge: "https://raw.githubusercontent.com/oskaars/Motorola123/943156290e64e61048f3e32a081cd990dee12e92/client/src/app/board/pawns/BlackPawn.svg?token=BHZ2SHK23OMCUUF7DU2EPX3HQ7OEU", x: 0, y: 1})
+
 export default function Chessboard() {
     let board = [];
 
     for(let j = verticalAxis.length-1; j >= 0; j--) {
         for (let i = 0; i < horizontalAxis.length; i++) {
             const number = j +i + 2;
+            let image = undefined;
 
-            board.push(<Tile number={number} />)
+            pieces.forEach(p => {
+            })
+
+            board.push(<Tile image={image} number={number} />)
         }
     }
     return <div className="bg-[#ff0000] w-[800px] h-[800px] grid grid-cols-8 text-black">{board}</div>
--- client/src/app/board/components/Tile.tsx
+++ client/src/app/board/components/Tile.tsx
@@ -1,12 +1,20 @@
 interface Props {
+    image?: string;
     number: number;
 }
 
-export default function Tile({ number }: Props) {
+export default function Tile({ number, image }: Props) {
     if(number % 2 === 0) {
-        return <div className="w-[100px] h-[100px] bg-[#a16f5a]"><img src="../pawns/BlackPawn.svg"/></div>
-    }
-    else {
-        return <div className="w-[100px] h-[100px] bg-[#ecd3b8]"></div>
+        return (
+            <div className="w-[100px] h-[100px] bg-[#a16f5a] grid place-content-center">
+            <img className="w-[80px]" src={image}/>
+            </div>
+        );
+    } else {
+        return (
+        <div className="w-[100px] h-[100px] bg-[#ecd3b8] grid place-content-center">
+        <img className="w-[80px]" src={image}/>
+        </div>
+        );
     }   
 }
