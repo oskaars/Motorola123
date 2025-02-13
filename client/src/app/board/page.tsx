2fa60b01a73b852af100d5da47204ac5f3f7d632
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2025-02-13 20:26:41 +0100

    implemented first version of algebraic notation

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,13 +1,26 @@
+"use client";
 import { NextPage } from 'next';
 import Chessboard from './components/Chessboard';
+import { useRef, useEffect } from 'react';
 
 const Board: NextPage = () => {
+    const chessboardRef = useRef<any>(null);
+
+    useEffect(() => {
+        (window as any).chess = {
+            move: (notation: string) => {
+                if (chessboardRef.current) {
+                    return chessboardRef.current.executeNotationMove(notation);
+                }
+                return false;
+            }
+        };
+    }, []);
+
     return (
-        <>
-            <div className="grid place-content-center h-screen bg-[#1a1a1a] pt-[5vh]">
-                <Chessboard />
-            </div>    
-        </>
+        <div className="grid place-content-center h-screen bg-[#1a1a1a] pt-[5vh]">
+            <Chessboard ref={chessboardRef} />
+        </div>    
     );
 };
 
