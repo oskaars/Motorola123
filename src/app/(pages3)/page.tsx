098199a7f64c659017b47d1cbfeb72487ebbd441
Ducky <duckymovess@gmail.com>
2025-03-30 21:29:22 +0200

    FOlder name fix

--- src/app/(pages3)/page.tsx
+++ src/app/(pages3)/page.tsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 import React from "react";
-import '/src/globals.css';
+import "/src/globals.css";
 import Multiplayer from "@/app/components/Multiplayer";
 import Chessboard from "@/app/components/Chessboard";
 
 const LandingPage: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-      <h1 className="text-4xl font-bold mb-8">Welcome to the Multiplayer App</h1>
-        <Multiplayer />
+      <h1 className="text-4xl font-bold mb-8">
+        Welcome to the Multiplayer App.
+      </h1>
+      <Multiplayer />
     </div>
   );
 };
